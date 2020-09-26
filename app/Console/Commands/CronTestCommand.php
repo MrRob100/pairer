f11a90ca95fd081a47193e7a39049cb33fa9a676
horidhenry <robarnold100@hotmail.com>
2020-09-26 10:08:14 +0100

    added test table for cron

---
+++ app/Console/Commands/CronTestCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CronTestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cron:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        DB::table('test_bin')->insert([
+            ['bin' => 'trash-'.rand()]
+        ]);
+
+        return 0;
+    }
+}
