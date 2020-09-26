bce546928568cbb91f831473c001cdf9e137fab5
horidhenry <robarnold100@hotmail.com>
2020-09-26 09:03:36 +0100

    typo in triggers table sorted

--- database/migrations/2020_09_26_075509_create_triggers_table.php
+++ database/migrations/2020_09_26_075509_create_triggers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTriggerTable extends Migration
+class CreateTriggersTable extends Migration
 {
     /**
      * Run the migrations.
