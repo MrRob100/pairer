b4bd2d9550bbe96ae5f53cf70209ee32c2fbf761
horidhenry <robarnold100@hotmail.com>
2020-12-04 07:22:44 +0000

    target seeder made

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserSeeder::class);
+        $this->call(TargetSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
