66d0ad591fd4ecb00d539bf3f96073a10addc850
horidhenry <robarnold100@hotmail.com>
2021-12-31 09:40:01 +0000

    empty pair check

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -30,13 +30,15 @@ class ResultsController extends Controller
 
             $data = array_values($this->pairDataService->getPairData($fakedRequest));
 
-            $results[] = [
-                'pair' => "$pair->s1$pair->s2",
-                'total_input' => $data[sizeof($data) -1]['input_symbol1_usd'] + $data[sizeof($data) -1]['input_symbol2_usd'],
-                'value' => $data[sizeof($data) -1]['balance_s1_usd'] + $data[sizeof($data) -1]['balance_s2_usd'],
-                'wbw' => $data[sizeof($data) -1]['wbw_usd_1'] + $data[sizeof($data) -1]['wbw_usd_2'],
-                'seconds' => Carbon::parse($data[sizeof($data) -1]['created_at'])->unix() - Carbon::parse($data[0]['created_at'])->unix(),
-            ];
+            if (sizeof($data) > 0) {
+                $results[] = [
+                    'pair' => "$pair->s1$pair->s2",
+                    'total_input' => $data[sizeof($data) -1]['input_symbol1_usd'] + $data[sizeof($data) -1]['input_symbol2_usd'],
+                    'value' => $data[sizeof($data) -1]['balance_s1_usd'] + $data[sizeof($data) -1]['balance_s2_usd'],
+                    'wbw' => $data[sizeof($data) -1]['wbw_usd_1'] + $data[sizeof($data) -1]['wbw_usd_2'],
+                    'seconds' => Carbon::parse($data[sizeof($data) -1]['created_at'])->unix() - Carbon::parse($data[0]['created_at'])->unix(),
+                ];
+            }
         }
 
         return $results;
