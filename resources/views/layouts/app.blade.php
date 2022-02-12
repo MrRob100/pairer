d3ebf2550e9bd76025d1a68516c1caec4442e5cc
horidhenry <robarnold100@hotmail.com>
2022-02-12 16:35:22 +0000

    secure asset logic removed as done globally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,14 @@
     <title>{{ env('APP_NAME') }}</title>
 
     <!-- Favicon-->
-    <link rel="icon" href="{{ env('APP_ENV') === 'local' ? asset('coin.png') : secure_asset('coin.png') }}" type="image/x-icon"/>
+    <link rel="icon" href="{{ asset('coin.png') }}" type="image/x-icon"/>
 
     <!-- Scripts -->
-    <script src="{{ env('APP_ENV') === 'local' ? asset('js/app.js') : secure_asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.4/js/jquery.dataTables.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.css">
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -22,7 +26,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
 
     <!-- Styles -->
-    <link href="{{ env('APP_ENV') === 'local' ? asset('css/app.css') : secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
